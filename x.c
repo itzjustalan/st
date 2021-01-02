4100656dcb02bc05311cd2a995b882ef5c7c85ea
devalan <devtesting1212@gmail.com>
2021-01-02 20:17:21 +0530

    middle button to right mouse button

--- x.c
+++ x.c
@@ -692,7 +692,10 @@ brelease(XEvent *e)
 
 	if (mouseaction(e, 1))
 		return;
-	if (e->xbutton.button == Button3)
+
+    if (e->xbutton.button == Button3)
+        selpaste(NULL);
+	if (e->xbutton.button == Button1)
 		mousesel(e, 1);
 }
 
