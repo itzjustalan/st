361717fe694be67440f82507aabc06d8073a9e73
itzjustalan <itzjustalan@gmail.com>
2021-02-22 22:21:39 +0530

    night owl didnt like opacity also added shift scroll

--- config.h
+++ config.h
@@ -132,16 +132,16 @@ static const char *colorname[] = {
 	"#cccccc",
 	"#555555",
   "#21222c",
-  "#011627",
 };
 //*/
+  // vs code night owl bg works best with 0 opacity "#011627",
 
 /*
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
 unsigned int defaultfg = 7;
-unsigned int defaultbg = 259;
+unsigned int defaultbg = 258;
 static unsigned int defaultcs = 256;
 static unsigned int defaultrcs = 257;
 
@@ -202,6 +202,8 @@ static MouseShortcut mshortcuts[] = {
 
 static Shortcut shortcuts[] = {
 	/* mask                 keysym          function        argument */
+	{ ShiftMask,            XK_Prior,       kscrollup,      {.i = 1} },
+	{ ShiftMask,            XK_Next,        kscrolldown,    {.i = 1} },
 	{ XK_ANY_MOD,           XK_Break,       sendbreak,      {.i =  0} },
 	{ ControlMask,          XK_Print,       toggleprinter,  {.i =  0} },
 	{ ShiftMask,            XK_Print,       printscreen,    {.i =  0} },
